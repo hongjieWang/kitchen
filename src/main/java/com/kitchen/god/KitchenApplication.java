6112cb7e1612448af570b584dc38e4255cc494ec
小猿 <julywhj@163.com>
2023-02-25 22:58:58 +0800

    修改工程名称

---
+++ src/main/java/com/kitchen/god/KitchenApplication.java
@@ -0,0 +1,18 @@
+package com.kitchen.god;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 造神平台
+ *
+ * @author wanghongjie
+ */
+@SpringBootApplication
+public class KitchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenApplication.class, args);
+    }
+
+}

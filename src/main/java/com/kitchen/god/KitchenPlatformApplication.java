ce63cae96bbb35f6090a8f3aa48e810a2e8830e0
小猿 <julywhj@163.com>
2023-02-25 22:46:31 +0800

    init

---
+++ src/main/java/com/kitchen/god/KitchenPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kitchen.god;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenPlatformApplication.class, args);
+	}
+
+}

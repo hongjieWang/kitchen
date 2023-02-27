c5ab99cb12dd2197817b2a51041411b4f70f5196
小猿 <julywhj@163.com>
2023-02-27 10:49:27 +0800

    引入mq

--- kitchen-demo/src/main/java/com/kitchen/demo/KitchenController.java
+++ kitchen-demo/src/main/java/com/kitchen/demo/KitchenController.java
@@ -1,6 +1,6 @@
 package com.kitchen.demo;
 
-import com.kitchen.god.sdk.MetricsClient;
+import com.kitchen.sdk.MetricsClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
